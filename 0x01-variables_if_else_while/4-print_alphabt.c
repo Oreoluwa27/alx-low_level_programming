7bbe989174021f9f7f00c37fb5dad7ac1922b827
akintomowo <akintomowooreoluwa@gmail.com>
2023-07-21 01:16:36 -0700

    new file

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,18 +8,16 @@
 
 int main(void)
 {
-	char ch, ch2;
-
+	char ch;
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
+		if (ch == 'q' || ch == 'e')
+		{
+			continue;
+		}
+
 		putchar(ch);
 	}
-
-	for (ch2 = 'A'; ch2 <= 'Z'; ch2++)
-	{
-		putchar(ch2);
-	}
-
 	putchar('\n');
 
 	return (0);
