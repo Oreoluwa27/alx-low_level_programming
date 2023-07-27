ac449edd5cecf18fc100dc3bc7472b72e6155b9f
akintomowo <akintomowooreoluwa@gmail.com>
2023-07-27 10:34:42 -0700

    newfile

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - print first 50 of fibonacci sequence
+ *
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	int i, sum = 0, a = 0, b = 1;
+
+	for (i = 1; i <= 50; i++)
+	{
+		sum = a + b;
+		a = b;
+		b = sum;
+		
+		if (i = 50)
+		{
+			printf("%d\n", sum);
+		}
+		else 
+		{
+			printf("%d, ", sum);
+		}
+	}
+
+	return (0);
+}
