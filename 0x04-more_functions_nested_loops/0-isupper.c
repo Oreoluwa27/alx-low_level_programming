47cdf515be531e07e3741761e572ca54a2160b77
akintomowo <akintomowooreoluwa@gmail.com>
2023-08-03 13:37:19 -0700

    newfile

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,12 +3,13 @@
 /**
  * _isupper - checks for uppercase character
  *
+ * @c: character to check
  * Return: 1 if uppercase 0 otherwise
  */
 
 int _isupper(int c)
 {
-	if(c >= 'A' && c <= 'Z')
+	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}
