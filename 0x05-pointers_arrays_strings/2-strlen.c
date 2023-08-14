6d7de56cdb6dd41e795f9477f4154f5fc4737fe6
akintomowo <akintomowooreoluwa@gmail.com>
2023-08-14 13:01:06 -0700

    update

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -11,9 +11,9 @@ int _strlen(char *s)
 {
 	int i, count;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; *(s + i) != '\0'; i++)
 	{
-		if (s[i] != ' ')
+		if (*(s + i) != ' ')
 		{
 			count++;
 		}
