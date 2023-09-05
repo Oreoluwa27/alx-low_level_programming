e51ac1aa2fbb45f982524bc357d334ece8b167aa
oreoluwa <akintomowooreoluwa@gmail.com>
2023-09-05 14:29:44 -0700

    newfle

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,15 +3,16 @@
 
 /**
  * main - prints it's name
- * @argc: arqument count 
+ * @argc: arqument count
  * @argv: argument vector
+ *
  * Return: 0 for success
  */
 
 int main(int __attribute__((unused)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
-	
+
 	return (0);
 
 }
