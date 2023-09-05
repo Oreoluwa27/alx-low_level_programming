d38646eae2c936093de1d26cfda3e1e51e5db3db
oreoluwa <akintomowooreoluwa@gmail.com>
2023-09-05 14:15:46 -0700

    newfile

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints it's name
+ * @argc: arqument count 
+ * @argv: argument vector
+ * Return: 0 for success
+ */
+
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	
+	return (0);
+
+}
