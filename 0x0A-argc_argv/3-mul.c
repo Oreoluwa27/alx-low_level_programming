424fa5f7fba3ab20e0c390e840c571a13435ad45
oreoluwa <akintomowooreoluwa@gmail.com>
2023-09-05 14:59:34 -0700

    newfile

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers.
+ * @argc: argument count
+ * @argv: arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int x, y = 1;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	for (x = 1; x < argc; x++)
+	{
+		y = y * atoi(argv[x]);
+	}
+	
+	printf("%d\n", y);
+
+	return (0);
+}
