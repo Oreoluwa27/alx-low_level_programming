0db1e45bb8f7bd306528f8d10ec7900efb5534e1
oreoluwa <akintomowooreoluwa@gmail.com>
2023-09-06 14:36:20 -0700

    update

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,7 +12,7 @@
 char *create_array(unsigned int size, char c)
 {
 	char *s;
-	int i;
+	unsigned int i;
 
 	if (size < 1)
 	{
@@ -25,7 +25,7 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
-	
+
 	for (i = 0; i < size; i++)
 	{
 		*(s + i) = c;
