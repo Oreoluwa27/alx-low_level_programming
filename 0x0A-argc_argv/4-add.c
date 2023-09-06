e4e4a70b45f299ca5c57703fb30b636d96cb7dc4
oreoluwa <akintomowooreoluwa@gmail.com>
2023-09-06 03:00:38 -0700

    newfile

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * main - adds positive number
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 for success, 1 for error
+ */
+
+int main(int argc, char **argv)
+{
+	int i, n, sum = 0;
+	char *flag;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		n = strtol(argv[i], &flag, 10);
+		if (*flag)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			sum += n;
+		}
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
+
+
