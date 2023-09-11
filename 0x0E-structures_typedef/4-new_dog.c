1eefbdd43e5f68d14ee82cba33e027a714c8a78f
oreoluwa <akintomowooreoluwa@gmail.com>
2023-09-11 16:14:16 -0700

    newfile

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,21 +13,55 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int i ,j, cnt;
 	dog_t *dog = malloc(sizeof(dog_t));
 
 	if (name == NULL || owner == NULL)
 	{
 		return (NULL);
 	}
-
 	if (dog == NULL)
 	{
 		return (NULL);
 	}
+	for (i  = 0; name[i] != '\0'; i++)
+	{
+		continue;
+	}
+	i++;	
 
-	dog->name = name;
+	dog->name = malloc(i * sizeof(char));
+
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	for (cnt = 0; cnt < i; cnt++)
+	{
+		dog->name[cnt] = name[cnt];
+	}
+
 	dog->age = age;
-	dog->owner = owner;
+
+	for (j  = 0; owner[i] != '\0'; j++)
+	{
+                continue;
+	}
+	j++;
+
+	dog->owner = malloc(j * sizeof(char));
+
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	for (cnt = 0; cnt < i; cnt++)
+	{
+		dog->owner[cnt] = owner[cnt];
+	}
 
 	return (dog);
 }
