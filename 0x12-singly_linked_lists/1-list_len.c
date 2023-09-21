3ebf5d26a21b9f1a15876c54ebd7ba41b227bab0
oreoluwa <akintomowooreoluwa@gmail.com>
2023-09-21 16:47:09 -0700

    update

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - count element in a list
+ *
+ * @h: start of the list
+ * Return: number of nodes
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t node_count = 0;
+
+	const list_t *temp = h;
+
+	while (temp != NULL)
+	{
+
+		temp = temp->next;
+
+	node_count++;
+	}
+
+	return node_count;
+}
